f6450bc5cd7cba92ff22c0dac892fcd0985afa71
Tynch <eloymartingimenez@gmail.com>
2025-10-09 11:14:58 -0300

    update files

---
+++ data/react-performance/memo-2.js
@@ -0,0 +1,68 @@
+//! APP
+function App() {
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark]
+  );
+
+  //? Memo whit an opject <<<<<<<<<<
+  const archiveOptions = useMemo(() => {
+    return {
+      show: true,
+      title: "Post archive in addition to main posts",
+    };
+  }, []);
+
+  return (
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
+
+      <PostProvider>
+        <Header />
+        <Main />
+        <Archive archiveOptions={archiveOptions} />
+        <Footer />
+      </PostProvider>
+    </section>
+  );
+}
+
+//! Meno
+const Archive = memo(function Archive({ archiveOptions }) {
+  const [posts] = useState(() =>
+    Array.from({ length: 10000 }, () => createRandomPost())
+  );
+
+  const [showArchive, setShowArchive] = useState(archiveOptions.show);
+
+  return (
+    <aside>
+      {/* Use the object */}
+      <h2>{archiveOptions.title}</h2>
+      <button onClick={() => setShowArchive((s) => !s)}>
+        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      </button>
+
+      {showArchive && (
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>
+              <p>
+                <strong>{post.title}:</strong> {post.body}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </aside>
+  );
+});
